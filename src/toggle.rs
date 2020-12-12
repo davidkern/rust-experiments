76b078e91970a14ee0476d4c8ceef328439ad965
David Kern <david@mju.io>
2020-12-11 20:07:06 -0800

    Generic actor types

--- src/toggle.rs
+++ src/toggle.rs
@@ -4,46 +4,105 @@ use tokio::sync::mpsc::{UnboundedSender, unbounded_channel, UnboundedReceiver};
 use std::ops::Deref;
 use crate::System;
 
-// INFINITE LOOP with types
-// Type definition toggles as the state toggles (!)
-pub type Receiver = State<UnboundedReceiver<State, _>>;
-pub struct Toggle(UnboundedSender<State<UnboundedReceiver<...>>>);
+//
+// IMPLEMENTATION
+//
 
-enum State<Receiver> {
-    Alpha(Receiver),
-    Beta(Receiver),
+pub struct Actor<Message, State> {
+    state: State,
+    receiver: Receiver<Message>,
 }
 
-impl Deref for Toggle {
-    type Target = UnboundedSender<State>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
+pub struct Mailbox<Message> {
+    actor: Sender<Message>
 }
 
-impl Toggle {
-    pub fn actor() -> (Toggle, State) {
+type Receiver<State> = UnboundedReceiver<State>;
+type Sender<State> = UnboundedSender<State>;
+
+impl<Message, State> Actor<Message, State> {
+    pub fn new_with_state(state: State) -> (Self, Mailbox<Message>) {
         let (sender, receiver) = unbounded_channel();
+        let actor = Self {
+            state,
+            receiver,
+        };
+        let mailbox = Mailbox::new_with_sender(sender);
 
-
-        (Toggle(sender), State::Alpha)
-    }
-
-    pub async fn start(&mut self) {
-        while let Some(msg) = self.recv().await {
-            self.state()
-        }
-    }
-
-    pub fn state(self) -> Self {
-        match self {
-            Self::Alpha => Self::Beta,
-            Self::Beta => Self::Alpha,
-        }
-    }
-
-    pub fn toggle(&self) -> &Self {
-
+        (actor, mailbox)
     }
 }
+
+impl<Message> Mailbox<Message> {
+    pub fn new_with_sender(sender: Sender<Message>) -> Self {
+        Self {
+            actor: sender,
+        }
+    }
+}
+
+//
+// USAGE
+//
+
+fn exercise_toggle() {
+
+}
+
+struct Toggle;
+
+enum ToggleState {
+    Alpha,
+    Beta,
+}
+
+impl Actor<Toggle, ToggleState> {
+    async fn toggle(&self) {
+    }
+}
+
+// // INFINITE LOOP with types
+// // Type definition toggles as the state toggles (!)
+// pub type Receiver = UnboundedReceiver<State>;
+// pub struct Toggle(UnboundedSender<State>);
+//
+// struct ToggleActor(Receiver);
+//
+// enum State {
+//     Alpha,
+//     Beta,
+// }
+//
+// impl Deref for Toggle {
+//     type Target = UnboundedSender<State>;
+//
+//     fn deref(&self) -> &Self::Target {
+//         &self.0
+//     }
+// }
+//
+// impl Toggle {
+//     pub fn actor() -> (Toggle, State) {
+//         let (sender, receiver) = unbounded_channel();
+//
+//
+//         (Toggle(sender), State::Alpha)
+//     }
+//
+//     pub async fn start(&mut self) {
+//         while let Some(msg) = self.recv().await {
+//             self.state()
+//         }
+//     }
+//
+//     pub fn state(self) -> Self {
+//         match self {
+//             Self::Alpha => Self::Beta,
+//             Self::Beta => Self::Alpha,
+//         }
+//     }
+//
+//     pub fn toggle(&self) -> &Self {
+//
+//     }
+// }
